e81468faf6ccf4927db62cd210df0912342c3f60
Martyna Rangl <martyna.rangl@yahoo.com>
2018-11-12 19:23:31 -0700

    adding J.Verne quoat, modifying js file, creating assets folder

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ const cardBox = document.querySelector('#card-box')
 
 cardBox.addEventListener('mouseover', function (e) {
     var card = e.target.parentElement.parentElement;
-    e.preventDefault();
     var text = document.createElement('h5');
     text.innerHTML = e.target.getAttribute('alt');
     text.classList.add('title')
     card.appendChild(text)
+    e.preventDefault();
+
 })
 cardBox.addEventListener('mouseout', function (e) {
-    var title = document.querySelector('.title')
+    var cardTitle = document.querySelector('.title')
     // console.log(e.target.parentElement)
     console.log(e.target.parentElement.parentElement)
-    e.target.parentElement.parentElement.removeChild(title)
+    e.target.parentElement.parentElement.removeChild(cardTitle)
     e.preventDefault()
 })
